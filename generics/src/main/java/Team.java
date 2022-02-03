69aa684d1cc060659135ca7fb45e291de7649cfb
ibrahimsever <isevertx@gmail.com>
2022-02-02 23:07:31 -0600

    Generics test

--- generics/src/main/java/Team.java
+++ generics/src/main/java/Team.java
@@ -11,6 +11,21 @@ public class Team {
 
     public String getName() {
         return name;
-
     }
+
+    public boolean addPlayer(Player player){
+        if(members.contains(player)){
+            System.out.println(player.getName()+ " is already in the team");
+            return false;
+        }else{
+            members.add(player);
+            return true;
+        }
+//TEst
+        }
+        public int numOfPlayer(){
+        return members.size();
+        }
+
+
 }
